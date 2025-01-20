90e1a27cbb0a832c2d4fdacbd26dbf806b949d41
mungdori <mungdori999@gmail.com>
2025-01-20 10:49:50 +0900

    ArrayStack Is_FULL

--- 02/ArrayStack/Test_ArrayStack.c
+++ 02/ArrayStack/Test_ArrayStack.c
@@ -1,31 +1,35 @@
 #include "ArrayStack.h"
 
-int main( void )
-{
-    int i= 0;
-    ArrayStack* Stack = NULL;
+int main(void) {
+    int i = 0;
+    ArrayStack *Stack = NULL;
 
     AS_CreateStack(&Stack, 10);
-    
+
     AS_Push(Stack, 3);
     AS_Push(Stack, 37);
     AS_Push(Stack, 11);
     AS_Push(Stack, 12);
+    AS_Push(Stack, 13);
+    AS_Push(Stack, 14);
+    AS_Push(Stack, 15);
+    AS_Push(Stack, 16);
+    AS_Push(Stack, 17);
+    AS_Push(Stack, 18);
+    printf("%d\n", AS_IsFull(Stack));
+    printf("Capacity: %d, Size: %d, Top: %d\n\n",
+           Stack->Capacity, AS_GetSize(Stack), AS_Top(Stack));
 
-    printf( "Capacity: %d, Size: %d, Top: %d\n\n", 
-        Stack->Capacity, AS_GetSize(Stack), AS_Top(Stack) );
-
-    for ( i=0; i<4; i++ )
-    {
-        if ( AS_IsEmpty(Stack) )
+    for (i = 0; i < 4; i++) {
+        if (AS_IsEmpty(Stack))
             break;
-        
-        printf( "Popped: %d, ", AS_Pop(Stack) );
-        
-        if ( ! AS_IsEmpty(Stack) )
-            printf( "Current Top: %d\n", AS_Top(Stack) );
+
+        printf("Popped: %d, ", AS_Pop(Stack));
+
+        if (!AS_IsEmpty(Stack))
+            printf("Current Top: %d\n", AS_Top(Stack));
         else
-            printf( "Stack Is Empty.\n" );
+            printf("Stack Is Empty.\n");
     }
 
     AS_DestroyStack(Stack);
