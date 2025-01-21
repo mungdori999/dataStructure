bdf61bf20d99ff65fff1f7dc03d2b1fca734f460
mungdori <mungdori999@gmail.com>
2025-01-21 11:55:31 +0900

    이진트리 실행

--- 03/CircularQueue/CircularQueue.c
+++ 03/CircularQueue/CircularQueue.c
@@ -1,44 +1,38 @@
 #include "CircularQueue.h"
 
-void  CQ_CreateQueue( CircularQueue** Queue, int Capacity)
-{
+void CQ_CreateQueue(CircularQueue **Queue, int Capacity) {
     //  큐를 자유저장소에 생성 
-    (*Queue )           = ( CircularQueue*)malloc(sizeof( CircularQueue ));
+    (*Queue) = (CircularQueue *) malloc(sizeof(CircularQueue));
 
     //  입력된 Capacity+1 만큼의 노드를 자유저장소에 생성 
-    (*Queue )->Nodes    = (Node*)malloc(sizeof(Node )* ( Capacity+1) );
+    (*Queue)->Nodes = (Node *) malloc(sizeof(Node) * (Capacity + 1));
 
-    (*Queue )->Capacity = Capacity;
-    (*Queue )->Front = 0;
-    (*Queue )->Rear  = 0;
+    (*Queue)->Capacity = Capacity;
+    (*Queue)->Front = 0;
+    (*Queue)->Rear = 0;
 }
 
-void CQ_DestroyQueue( CircularQueue* Queue )
-{
+void CQ_DestroyQueue(CircularQueue *Queue) {
     free(Queue->Nodes);
-    free(Queue );
+    free(Queue);
 }
 
-void CQ_Enqueue( CircularQueue* Queue, ElementType Data)
-{
-    int Position=0;
-  
-    if(Queue->Rear==Queue->Capacity)
-    {
-        Position=Queue->Rear;
-        Queue->Rear=0;
-    }
-    else
-        Position=Queue->Rear++;
-  
-    Queue->Nodes[Position].Data=Data;
+void CQ_Enqueue(CircularQueue *Queue, ElementType Data) {
+    int Position = 0;
+
+    if (Queue->Rear == Queue->Capacity) {
+        Position = Queue->Rear;
+        Queue->Rear = 0;
+    } else
+        Position = Queue->Rear++;
+
+    Queue->Nodes[Position].Data = Data;
 }
 
-ElementType CQ_Dequeue( CircularQueue* Queue )
-{
+ElementType CQ_Dequeue(CircularQueue *Queue) {
     int Position = Queue->Front;
 
-    if ( Queue->Front == Queue->Capacity )
+    if (Queue->Front == Queue->Capacity)
         Queue->Front = 0;
     else
         Queue->Front++;
@@ -46,23 +40,20 @@ ElementType CQ_Dequeue( CircularQueue* Queue )
     return Queue->Nodes[Position].Data;
 }
 
-int CQ_GetSize( CircularQueue* Queue )
-{
-    if ( Queue->Front <= Queue->Rear )
+int CQ_GetSize(CircularQueue *Queue) {
+    if (Queue->Front <= Queue->Rear)
         return Queue->Rear - Queue->Front;
     else
         return Queue->Rear + (Queue->Capacity - Queue->Front) + 1;
 }
 
-int CQ_IsEmpty( CircularQueue* Queue )
-{
+int CQ_IsEmpty(CircularQueue *Queue) {
     return (Queue->Front == Queue->Rear);
 }
 
-int CQ_IsFull( CircularQueue* Queue )
-{
-    if ( Queue->Front < Queue->Rear )
-        return ( Queue->Rear - Queue->Front) == Queue->Capacity;
+int CQ_IsFull(CircularQueue *Queue) {
+    if (Queue->Front < Queue->Rear)
+        return (Queue->Rear - Queue->Front) == Queue->Capacity;
     else
-        return ( Queue->Rear + 1 ) == Queue->Front;
+        return (Queue->Rear + 1) == Queue->Front;
 }
---
+++ 04/LCRSTree/LCRSTree.c
@@ -0,0 +1,74 @@
+#include "LCRSTree.h"
+
+LCRSNode *LCRS_CreateNode(ElementType NewData) {
+    LCRSNode *NewNode = (LCRSNode *) malloc(sizeof(LCRSNode));
+    NewNode->LeftChild = NULL;
+    NewNode->RightSibling = NULL;
+    NewNode->Data = NewData;
+
+    return NewNode;
+}
+
+void LCRS_DestroyNode(LCRSNode *Node) {
+    free(Node);
+}
+
+void LCRS_DestroyTree(LCRSNode *Root) {
+    if (Root->RightSibling != NULL)
+        LCRS_DestroyTree(Root->RightSibling);
+
+    if (Root->LeftChild != NULL)
+        LCRS_DestroyTree(Root->LeftChild);
+
+    Root->LeftChild = NULL;
+    Root->RightSibling = NULL;
+
+    LCRS_DestroyNode(Root);
+}
+
+void LCRS_AddChildNode(LCRSNode *Parent, LCRSNode *Child) {
+    if (Parent->LeftChild == NULL) {
+        Parent->LeftChild = Child;
+    } else {
+        LCRSNode *TempNode = Parent->LeftChild;
+        while (TempNode->RightSibling != NULL)
+            TempNode = TempNode->RightSibling;
+
+        TempNode->RightSibling = Child;
+    }
+}
+
+void LCRS_PrintTree(LCRSNode *Node, int Depth) {
+    // 들여쓰기
+    int i = 0;
+    for (i = 0; i < Depth - 1; i++)
+        printf("   "); // 공백 3칸
+
+    if (Depth > 0) // 자식노드 여부 표시
+        printf("+--");
+
+    // 노드 데이터 출력
+    printf("%c\n", Node->Data);
+
+    if (Node->LeftChild != NULL)
+        LCRS_PrintTree(Node->LeftChild, Depth + 1);
+
+    if (Node->RightSibling != NULL)
+        LCRS_PrintTree(Node->RightSibling, Depth);
+}
+
+void LCRS_PrintNodesAtLevel(LCRSNode *Node, int Level) {
+    if (Node == NULL) {
+        return;
+    }
+    if (Level == 0) {
+        printf("%c ", Node->Data);
+        LCRS_PrintNodesAtLevel(Node->RightSibling, Level);
+    } else {
+        // 자식 노드를 탐색하며 레벨을 1 감소
+        LCRS_PrintNodesAtLevel(Node->LeftChild, Level - 1);
+        LCRS_PrintNodesAtLevel(Node->RightSibling, Level);
+    }
+
+}
+
